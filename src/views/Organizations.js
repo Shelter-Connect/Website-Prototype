c25ee158c1b32f598b70cd6b710645ce148bd476
Roman <alonzofroman@gmail.com>
2022-01-14 15:12:13 -0800

    Org page css linked

--- src/views/Organizations.js
+++ src/views/Organizations.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../organizations.css';
 
 import OrgNavbar from 'components/OrgNavbar.js';
 import Footer from 'components/Footer.js';
@@ -9,8 +10,15 @@ export default function Organizations() {
         <>
         <OrgNavbar />
         <main>
-            <div className='relative pt-16 pb-32 flex content-center items-center justify-center' style={{ minHeight: '87vh' }}>
-
+            <div className='relative pt-16 pb-32 flex content-center justify-center' style={{ minHeight: '87vh' }}>
+                <div className='orgSection'>
+                    <div className='orgHeader'>
+                        <h2>List of Organizations</h2>
+                    </div>
+                    <div className='orgList'>
+                        
+                    </div>
+                </div>
             </div>
         </main>
         <Footer />
