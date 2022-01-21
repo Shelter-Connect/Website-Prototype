58bb6ab1cce739d94b15b6aed679e4922329a4a0
Netapp DPG <netappdpg@Netapps-Air.lan>
2022-01-20 16:25:29 -0800

    added a table component needed for large volume of data

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import Landing from "views/Landing.js";
+import Datatable from "./components/DataTable";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path='/' component={Landing} />
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/demo' component={Datatable} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
