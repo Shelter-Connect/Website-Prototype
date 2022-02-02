e6357608edae9bf1252efc314c2a7f5cd51eee4b
Roman <alonzofroman@gmail.com>
2022-02-02 13:44:30 -0800

    Set a different icon for food drive orgs

--- src/components/OrgCards.js
+++ src/components/OrgCards.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // CSS
 import "../organizations.css";
 
 // React Icons
-import { BsGlobe } from 'react-icons/bs';
+import { BsGlobe } from "react-icons/bs";
+import { FaCarrot } from "react-icons/fa";
 
 // Firestore methods
 import firebase from "firebase";
@@ -13,7 +14,7 @@ import { db } from "../firebase";
 
 function OrgCards() {
   const [orgs, setOrgs] = React.useState([]);
-// This makes the call to the database for all organizations
+  // This makes the call to the database for all organizations
   useEffect(() => {
     const q = query(collection(db, "organizations"));
     onSnapshot(q, (querySnapshot) => {
@@ -28,25 +29,53 @@ function OrgCards() {
 
   console.log(orgs);
 
-
   return (
     <div className='orgCards'>
-      {/* Going over each item in the organization list to create a card element */}
-        {orgs.filter(orgs => orgs.data.name !== 'Linkare Test Organization').map((organization) => (
-
-          <div className='orgCardSingle border border-solid' key={organization.data.name}>
-              <h4 className='orgCardHeader' style={{ display: 'flex',  alignItems: 'center', justifyContent: 'center' }}> <BsGlobe size={30} style={{ paddingRight: '5px', color: 'blue'}}/> {organization.data.name}</h4>
-              <p className='orgDescription'>{organization.data.description}</p>              
-              <p className='orgAddress'>Address: {organization.data.address ? `${organization.data.address}` : 'None available'}</p>
-{/* This link leads to a URL that we can use to have our single org page.  */}
-              <Link to={`/orgs/${organization.id}`} className='orgLink'>View Organization</Link>
-
+      {/* Going over each item in the organization list to create a card element, filtering out the test org */}
+      {orgs
+        .filter((orgs) => orgs.data.name !== "Linkare Test Organization")
+        .map((organization) => (
+          <div
+            className='orgCardSingle border border-solid'
+            key={organization.data.name}
+          >
+            <h4
+              className='orgCardHeader'
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {/* Render Icon, Carrot for food drive, globe for others */}
+              {organization.data.foodDrive === false ? (
+                <BsGlobe
+                  size={30}
+                  style={{ paddingRight: "5px", color: "blue" }}
+                />
+              ) : (
+                <FaCarrot
+                  size={30}
+                  style={{ paddingRight: "5px", color: "blue" }}
+                />
+              )}
+              {organization.data.name}
+            </h4>
+            <p className='orgDescription'>{organization.data.description}</p>
+            <p className='orgAddress'>
+              Address:{" "}
+              {organization.data.address
+                ? `${organization.data.address}`
+                : "None available"}
+            </p>
+            {/* This link leads to a URL that we can use to have our single org page.  */}
+            <Link to={`/orgs/${organization.id}`} className='orgLink'>
+              View Organization
+            </Link>
           </div>
-
         ))}
-      
     </div>
   );
-}; 
+}
 
 export default OrgCards;
