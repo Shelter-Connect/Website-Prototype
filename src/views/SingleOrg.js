b60b333bfd044dccc1dc2f9fd97aed31121f1cbc
Roman <alonzofroman@gmail.com>
2022-02-28 13:52:53 -0800

    Configured useparams to grab org id on page load

--- src/views/SingleOrg.js
+++ src/views/SingleOrg.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from 'react-router-dom';
 import firebase from 'firebase';
 import { onSnapshot, collection, doc, query } from 'firebase/firestore';
 import { db } from '../firebase';
@@ -7,6 +8,11 @@ import OrgNavbar from "../components/OrgNavbar";
 import Footer from "../components/Footer";
 
 export default function SingleOrg() {
+    const [org, setOrg] = React.useState([]);
+
+    const { id } = useParams();
+    console.log(id);
+
   return (
     <>
       <OrgNavbar />
