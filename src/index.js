c32dac6d3a6ced2ab404d17e7c388a3ccf71df23
Roman <alonzofroman@gmail.com>
2022-02-28 13:39:09 -0800

    Added single org route to index

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import Landing from "views/Landing.js";
 import Organizations from "views/Organizations.js";
+import SingleOrg from 'views/SingleOrg';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Landing} />
       <Route exact path='/orgs' component={Organizations} />
+      <Route exact path='/orgs/:id' component={SingleOrg} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
--- src/views/SingleOrg.js
+++ src/views/SingleOrg.js
@@ -1,4 +1,17 @@
-import React from 'react';
+import React from "react";
+import firebase from 'firebase';
+import { onSnapshot, collection, doc, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
-import OrgNavbar from '../components/OrgNavbar';
-import Footer from '../components/Footer';
+import OrgNavbar from "../components/OrgNavbar";
+import Footer from "../components/Footer";
+
+export default function SingleOrg() {
+  return (
+    <>
+      <OrgNavbar />
+
+      <Footer />
+    </>
+  );
+}
