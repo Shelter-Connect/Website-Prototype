5ce3335feaf0cbf313bb9787b48908631c172be6
Roman <alonzofroman@gmail.com>
2022-03-02 14:28:24 -0800

    Org data able to render from firestore

--- src/views/SingleOrg.js
+++ src/views/SingleOrg.js
@@ -1,42 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
-import firebase from 'firebase';
-import { onSnapshot, collection, doc, query, firestore } from 'firebase/firestore';
-import { db } from '../firebase';
-
+import { useParams } from "react-router-dom";
+import firebase from "firebase";
+import {
+  onSnapshot,
+  collection,
+  doc,
+  query,
+  firestore,
+} from "firebase/firestore";
+import { db } from "../firebase";
 import OrgNavbar from "../components/OrgNavbar";
 import Footer from "../components/Footer";
 
 export default function SingleOrg() {
-
   // Grab org ID from URL parameters
-    const { id } = useParams();
-    console.log(id);
+  const { id } = useParams();
+  // console.log(id);
 
-// Makes call to organization documents in firestore
-    const [orgData, setOrgData] = React.useState([]);
-    useEffect(() => {
-      const q = query(collection(db, 'organizations'));
-      onSnapshot(q, (querySnapshot) => {
-        setOrgData(
-          querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            data: doc.data(),
-          }))
-        )
-      })
-    }, []);
-    console.log(orgData);
-    // Filters out the single org by the id in the firestore
-    const thisOrgData = orgData.filter((orgData) => orgData.id === id);
-    console.log(thisOrgData)
+  // Makes call to organization documents in firestore
+  const [orgData, setOrgData] = React.useState([]);
+  useEffect(() => {
+    const q = query(collection(db, "organizations"));
+    onSnapshot(q, (querySnapshot) => {
+      setOrgData(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+  // console.log(orgData);
 
   return (
     <>
       <OrgNavbar />
-        <div className='singleOrgInfo'>
-          
+      <main>
+        <div
+          className='relative pt-16 pb-32 flex content-center justify-center'
+          style={{ minHeight: "87vh" }}
+        >
+          {orgData
+            .filter((orgData) => orgData.id === id)
+            .map((organization) => (
+
+              <div className='singleOrginfo' key={organization.id}>
+                <h2>{organization.data.name}</h2>
+              </div>
+
+            ))}
         </div>
+      </main>
+
       <Footer />
     </>
   );
