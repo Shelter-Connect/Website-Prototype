3a96abcf01ebbfa7bf685746215fd6e73e79ec8b
Roman <alonzofroman@gmail.com>
2022-03-02 14:11:19 -0800

    Now correctly grabbing single org data from firestore by its id in firestore

--- src/views/SingleOrg.js
+++ src/views/SingleOrg.js
@@ -1,22 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import firebase from 'firebase';
-import { onSnapshot, collection, doc, query } from 'firebase/firestore';
+import { onSnapshot, collection, doc, query, firestore } from 'firebase/firestore';
 import { db } from '../firebase';
 
 import OrgNavbar from "../components/OrgNavbar";
 import Footer from "../components/Footer";
 
 export default function SingleOrg() {
-    const [org, setOrg] = React.useState([]);
 
+  // Grab org ID from URL parameters
     const { id } = useParams();
     console.log(id);
+
+// Makes call to organization documents in firestore
+    const [orgData, setOrgData] = React.useState([]);
+    useEffect(() => {
+      const q = query(collection(db, 'organizations'));
+      onSnapshot(q, (querySnapshot) => {
+        setOrgData(
+          querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      })
+    }, []);
+    console.log(orgData);
+    // Filters out the single org by the id in the firestore
+    const thisOrgData = orgData.filter((orgData) => orgData.id === id);
+    console.log(thisOrgData)
 
   return (
     <>
       <OrgNavbar />
-
+        <div className='singleOrgInfo'>
+          
+        </div>
       <Footer />
     </>
   );
