0b55404528c4f81346e50bdaf486497344a46303
Roman <alonzofroman@gmail.com>
2022-03-07 12:39:04 -0800

    Styling of single org page

--- src/views/SingleOrg.js
+++ src/views/SingleOrg.js
@@ -30,7 +30,7 @@ export default function SingleOrg() {
       );
     });
   }, []);
-  console.log(orgData);
+  // console.log(orgData);
 
   return (
     <>
@@ -43,26 +43,58 @@ export default function SingleOrg() {
           {orgData
             .filter((orgData) => orgData.id === id)
             .map((organization) => (
+              <div
+                className='singleOrginfo'
+                style={{ textAlign: "center" }}
+                key={organization.id}
+              >
+                <h2 style={{ fontWeight: "bold", fontSize: "x-large" }}>
+                  {organization.data.name}
+                </h2>
+                <p style={{ fontSize: "large" }}>
+                  Address: {organization.data.address ? `${organization.data.address}` : 'None Available'}
+                </p>
 
-              <div className='singleOrginfo' style={{ textAlign: 'center' }} key={organization.id}>
-                <h2 style={{ fontWeight: 'bold' }}>{organization.data.name}</h2>
-                <p>Address: {organization.data.address}</p>
-
-                <div className='singleOrgAbout'>
-                  <p>{organization.data.description}</p>
-                  <a href={organization.data.donationLink}>{organization.data.donationLink}</a>
-                  <p>{organization.data.email}</p>
+                <div
+                  className='singleOrgAbout'
+                  style={{ marginTop: "15px", marginBottom: "15px", border: 'solid', borderWidth: '2px', marginRight: '10px', marginLeft: '10px' }}
+                >
+                  <p style={{ fontSize: "large", paddingLeft: "15px", paddingRight: '15px', marginBottom: '10px' }}>
+                    {organization.data.description}
+                  </p>
+                  <a
+                    href={organization.data.donationLink}
+                    style={{ color: "blue", fontSize: "large" }}
+                  >
+                    {organization.data.donationLink}
+                  </a>
+                  <p style={{ marginTop: "10px", fontSize: "large" }}>
+                    Email: {organization.data.email}
+                  </p>
+                  <p style={{ fontSize: 'large', marginTop: '10px' }}>
+                    Phone: {organization.data.number ? `${organization.data.number}` : 'None Available'}
+                  </p>
                 </div>
 
-              <div className='orgrequests'>
-                <h3>Requests</h3>
-                {organization.data.itemCategories.map((itemRequests) => (
-                  <p key={itemRequests.index}>{itemRequests}</p>
-                ))}
+                <div className='orgRequests'>
+                  <h3
+                    style={{
+                      fontWeight: "bold",
+                      fontSize: "x-large",
+                      marginBottom: "10px",
+                    }}
+                  >
+                    Requests
+                  </h3>
+                  <div className='flex content-center, justify-center'>
+                    {organization.data.itemCategories.map((itemRequests) => (
+                      <p key={itemRequests.index} style={{ fontSize: "large", padding: '10px' }}>
+                        {itemRequests}
+                      </p>
+                    ))}
+                  </div>
+                </div>
               </div>
-
-              </div>
-
             ))}
         </div>
       </main>
