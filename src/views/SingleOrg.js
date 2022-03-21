4afd6ac0266ec247a3939fc6190f55533a3f68a7
Roman <alonzofroman@gmail.com>
2022-03-21 12:22:25 -0700

    Added comments to code

--- src/views/SingleOrg.js
+++ src/views/SingleOrg.js
@@ -40,6 +40,7 @@ export default function SingleOrg() {
           className='relative pt-16 pb-32 flex content-center justify-center'
           style={{ minHeight: "87vh" }}
         >
+          {/* Finds org information by org ID, taken from URL */}
           {orgData
             .filter((orgData) => orgData.id === id)
             .map((organization) => (
@@ -52,14 +53,31 @@ export default function SingleOrg() {
                   {organization.data.name}
                 </h2>
                 <p style={{ fontSize: "large" }}>
-                  Address: {organization.data.address ? `${organization.data.address}` : 'None Available'}
+                  Address:{" "}
+                  {organization.data.address
+                    ? `${organization.data.address}`
+                    : "None Available"}
                 </p>
 
                 <div
                   className='singleOrgAbout'
-                  style={{ marginTop: "15px", marginBottom: "15px", border: 'solid', borderWidth: '2px', marginRight: '10px', marginLeft: '10px' }}
+                  style={{
+                    marginTop: "15px",
+                    marginBottom: "15px",
+                    border: "solid",
+                    borderWidth: "2px",
+                    marginRight: "10px",
+                    marginLeft: "10px",
+                  }}
                 >
-                  <p style={{ fontSize: "large", paddingLeft: "15px", paddingRight: '15px', marginBottom: '10px' }}>
+                  <p
+                    style={{
+                      fontSize: "large",
+                      paddingLeft: "15px",
+                      paddingRight: "15px",
+                      marginBottom: "10px",
+                    }}
+                  >
                     {organization.data.description}
                   </p>
                   <a
@@ -71,31 +89,80 @@ export default function SingleOrg() {
                   <p style={{ marginTop: "10px", fontSize: "large" }}>
                     Email: {organization.data.email}
                   </p>
-                  <p style={{ fontSize: 'large', marginTop: '10px' }}>
-                    Phone: {organization.data.number ? `${organization.data.number}` : 'None Available'}
+                  <p style={{ fontSize: "large", marginTop: "10px" }}>
+                    Phone:{" "}
+                    {organization.data.number
+                      ? `${organization.data.number}`
+                      : "None Available"}
                   </p>
-                  <div className='schedule' style={{ marginTop: '10px' }}>
+                  <div className='schedule' style={{ marginTop: "10px" }}>
                     {/* If schedule exists, starts process for each day */}
                     {/* Parsing formula is organization.data.schedule.Frday[0].toDate().getHours() */}
-                     
-
-                    {!organization.data.schedule ? (<p>No Schedule Available</p>) : (<p>
-                      Schedule: <br />
-                      { organization.data.schedule.Monday[0] ? `Monday: ${organization.data.schedule.Monday[0].toDate().getHours()}00 - ${organization.data.schedule.Monday[1].toDate().getHours()}00` : 'Monday: Closed/Unavailable' } <br />
-                    { organization.data.schedule.Tuesday[0] ? `Tuesday: ${organization.data.schedule.Tuesday[0].toDate().getHours()}00 - ${organization.data.schedule.Tuesday[1].toDate().getHours()}00` : 'Tuesday: Closed/Unavailable' } <br />
-                    { organization.data.schedule.Wednesday[0] ? `Wednesday: ${organization.data.schedule.Wednesday[0].toDate().getHours()}00 - ${organization.data.schedule.Wednesday[1].toDate().getHours()}00` : 'Wednesday: Closed/Unavailable' } <br />
-                    { organization.data.schedule.Thursday[0] ? `Thursday: ${organization.data.schedule.Thursday[0].toDate().getHours()}00 - ${organization.data.schedule.Thursday[1].toDate().getHours()}00` : 'Thursday: Closed/Unavailable' } <br />
-                    { organization.data.schedule.Friday[0] ? `Friday: ${organization.data.schedule.Friday[0].toDate().getHours()}00 - ${organization.data.schedule.Friday[1].toDate().getHours()}00` : 'Friday: Closed/Unavailable' } <br />
-                    { organization.data.schedule.Saturday[0] ? `Saturday: ${organization.data.schedule.Saturday[0].toDate().getHours()}00 - ${organization.data.schedule.Saturday[1].toDate().getHours()}00` : 'Saturday: Closed/Unavailable' } <br />
-                    { organization.data.schedule.Sunday[0] ? `Sunday: ${organization.data.schedule.Sunday[0].toDate().getHours()}00 - ${organization.data.schedule.Sunday[1].toDate().getHours()}00` : 'Sunday: Closed/Unavailable' } <br />
-
-                    </p>) }
-
                     
-                   
-                    
-                  
-                    </div>
+                    {!organization.data.schedule ? (
+                      <p>No Schedule Available</p>
+                    ) : (
+                      <p>
+                        Schedule: <br />
+                        {organization.data.schedule.Monday[0]
+                          ? `Monday: ${organization.data.schedule.Monday[0]
+                              .toDate()
+                              .getHours()}00 - ${organization.data.schedule.Monday[1]
+                              .toDate()
+                              .getHours()}00`
+                          : "Monday: Closed/Unavailable"}{" "}
+                        <br />
+                        {organization.data.schedule.Tuesday[0]
+                          ? `Tuesday: ${organization.data.schedule.Tuesday[0]
+                              .toDate()
+                              .getHours()}00 - ${organization.data.schedule.Tuesday[1]
+                              .toDate()
+                              .getHours()}00`
+                          : "Tuesday: Closed/Unavailable"}{" "}
+                        <br />
+                        {organization.data.schedule.Wednesday[0]
+                          ? `Wednesday: ${organization.data.schedule.Wednesday[0]
+                              .toDate()
+                              .getHours()}00 - ${organization.data.schedule.Wednesday[1]
+                              .toDate()
+                              .getHours()}00`
+                          : "Wednesday: Closed/Unavailable"}{" "}
+                        <br />
+                        {organization.data.schedule.Thursday[0]
+                          ? `Thursday: ${organization.data.schedule.Thursday[0]
+                              .toDate()
+                              .getHours()}00 - ${organization.data.schedule.Thursday[1]
+                              .toDate()
+                              .getHours()}00`
+                          : "Thursday: Closed/Unavailable"}{" "}
+                        <br />
+                        {organization.data.schedule.Friday[0]
+                          ? `Friday: ${organization.data.schedule.Friday[0]
+                              .toDate()
+                              .getHours()}00 - ${organization.data.schedule.Friday[1]
+                              .toDate()
+                              .getHours()}00`
+                          : "Friday: Closed/Unavailable"}{" "}
+                        <br />
+                        {organization.data.schedule.Saturday[0]
+                          ? `Saturday: ${organization.data.schedule.Saturday[0]
+                              .toDate()
+                              .getHours()}00 - ${organization.data.schedule.Saturday[1]
+                              .toDate()
+                              .getHours()}00`
+                          : "Saturday: Closed/Unavailable"}{" "}
+                        <br />
+                        {organization.data.schedule.Sunday[0]
+                          ? `Sunday: ${organization.data.schedule.Sunday[0]
+                              .toDate()
+                              .getHours()}00 - ${organization.data.schedule.Sunday[1]
+                              .toDate()
+                              .getHours()}00`
+                          : "Sunday: Closed/Unavailable"}{" "}
+                        <br />
+                      </p>
+                    )}
+                  </div>
                 </div>
 
                 <div className='orgRequests'>
@@ -110,7 +177,10 @@ export default function SingleOrg() {
                   </h3>
                   <div className='flex content-center, justify-center'>
                     {organization.data.itemCategories.map((itemRequests) => (
-                      <p key={itemRequests.index} style={{ fontSize: "large", padding: '10px' }}>
+                      <p
+                        key={itemRequests.index}
+                        style={{ fontSize: "large", padding: "10px" }}
+                      >
                         {itemRequests}
                       </p>
                     ))}
